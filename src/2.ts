facbe15fb5413e42ad932790bee6b638e30a8264
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2023-10-06 10:29:39 +0300

    task-3

--- src/2.ts
+++ src/2.ts
@@ -16,24 +16,30 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  name: string
+  department: string
+  salary: number
 
   constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
+    this.name = name
+    this.department = department
+    this.salary = salary
   }
 
   getEmployeeDetails() {
-    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`
   }
 }
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000)
+  }
 }
 
+const employee1 = new Employee("John", "HR", 50000)
+const manager1 = new Manager("Alice", "Management", 60000)
 
-export {};
+console.log(employee1.getEmployeeDetails())
+console.log(manager1.getEmployeeDetails())
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,66 @@
-const key = new Key();
+// Клас Key
+class Key {
+  private signature: number
+  constructor() {
+    this.signature = Math.random()
+  }
 
-const house = new MyHouse(key);
-const person = new Person(key);
+  getSignature(): number {
+    return this.signature
+  }
+}
 
-house.openDoor(person.getKey());
+// Клас Person
+class Person {
+  private key: Key
 
-house.comeIn(person);
+  constructor(key: Key) {
+    this.key = key
+  }
 
+  getKey(): Key {
+    return this.key
+  }
+}
 
-export {};
+// Абстрактний клас House
+abstract class House {
+  protected door: boolean = false
+  protected tenants: Person[] = []
+  protected key: Key
+
+  constructor(key: Key) {
+    this.key = key
+  }
+
+  abstract openDoor(key: Key): void
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person)
+      console.log(`${person.getKey().getSignature()} entered the house.`)
+    } else {
+      console.log("The door is closed. Cannot enter.")
+    }
+  }
+}
+
+// Клас MyHouse успадковує абстрактний клас House
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true
+      console.log("The door is open.")
+    } else {
+      this.door = false
+      console.log("Invalid key. The door remains closed.")
+    }
+  }
+}
+
+const key = new Key()
+const house = new MyHouse(key)
+const person = new Person(key)
+
+house.openDoor(person.getKey())
+house.comeIn(person)
